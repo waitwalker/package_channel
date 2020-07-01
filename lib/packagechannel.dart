545cbd04924a5f064622783261602a343045a4d0
waitwalker <waitwalker@163.com>
2020-07-01 09:54:30 +0800

    渠道打包插件获取

---
+++ lib/packagechannel.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+
+class Packagechannel {
+  static const MethodChannel _channel =
+      const MethodChannel('packagechannel');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+  static Future<String> get getChannel async {
+    if(Platform.isAndroid) {
+      final String version = await _channel.invokeMethod('getChannel');
+      return version;
+    }
+    return 'AppStore';
+  }
+}
